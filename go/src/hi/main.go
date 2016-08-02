2689b43d0b68c7f417a4dd415406fce4f65c53a3
David Fink <dtfink8499@gmail.com>
2016-08-02 19:51:35 -0400

    save

--- go/src/hi/main.go
+++ go/src/hi/main.go
@@ -9,5 +9,5 @@ import (
 func main() {
 	fmt.Print(c.Clear)
 	name := input.Ask(c.Blue + "Hi, what's your name?  " + c.X + c.Red)
-	fmt.Println(c.X + c.Blue + "Nice to meet you," + name + c.X)
+	fmt.Println(c.X + c.Blue + "Nice to meet you, " + name + c.X)
 }
