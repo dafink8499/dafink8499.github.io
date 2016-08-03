d9705ede2e5e028e1ebb5d74caae0b5f21f18b9e
David Fink <dtfink8499@gmail.com>
2016-08-02 20:43:28 -0400

    save

--- go/src/waffles/main.go
+++ go/src/waffles/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	fmt.Print(c.Clear)
 	fmt.Println(c.Red + "Welcome to Waffles" + c.X)
-	waffles := i.Ask(c.Blue + "Do you like Waffles? " + c.X)
+	waffles := i.Ask(c.Blue + "Do you like Waffles? " + c.Red)
 	if waffles == "yes" {
-		if pancakes := i.Ask(c.Blue + "Do you like pancakes? "); pancakes == "yes" {
-			if toast := i.Ask(c.Blue + "Do you like french toast? "); toast == "yes" {
+		if pancakes := i.Ask(c.Blue + "Do you like pancakes? " + c.Red); pancakes == "yes" {
+			if toast := i.Ask(c.Blue + "Do you like french toast? " + c.Red); toast == "yes" {
 				fmt.Println(c.Blue + "Can't wait to get a mouthful")
 			}
 		}
